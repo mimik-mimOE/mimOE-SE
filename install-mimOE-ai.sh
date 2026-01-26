c02da0b27d0cd5aaa9d1b104563f248f9d0e35d2
Chih Chung Hsu <jeremy.hsu@mimik.com>
2026-01-26 13:57:01 -0800

    renaming

--- install-mimOE-ai.sh
+++ install-mimOE-ai.sh
@@ -28,13 +28,8 @@ LOCAL_ADDON_URL="${LOCAL_HTTP_BASE}/mimOE-addon-ai-foundation/ai-foundation-1.6.
 # Remote URLs (production)
 PROD_MACOS_ARM64_URL="https://github.com/mimik-mimOE/mimOE-SE/releases/download/v${VERSION}/mimOE-ai-SE-macOS-developer-ARM64-v${VERSION}.zip"
 PROD_LINUX_AMD64_URL="https://github.com/mimik-mimOE/mimOE-SE/releases/download/v${VERSION}/mimOE-ai-SE-linux-developer-X86_64-VULKAN-v${VERSION}.tar"
-<<<<<<< HEAD
-PROD_LINUX_ARM64_URL="https://github.com/mimik-mimOE/mimOE-SE/releases/download/v${VERSION}/mimOE-ai-SE-linux-developer-ARM64-CUDA-v${VERSION}.tar"
-PROD_ADDON_URL="https://github.com/mimik-mimOE/mimOE-addon-ai-foundation-/releases/download/v1.6.1/ai-foundation-1.6.1.addon"
-=======
 PROD_LINUX_ARM64_URL="https://github.com/mimik-mimOE/mimOE-SE/releases/download/v${VERSION}/mimOE-ai-SE-linux-developer-ARM64-v${VERSION}.tar"
 PROD_ADDON_URL="https://github.com/mimik-mimOE/mimOE-addon-ai-foundation/releases/download/v1.6.1/ai-foundation-1.6.1.addon"
->>>>>>> dc8fab0 (renaming)
 
 # Select URLs based on mode
 if [ "$LOCAL_HTTP" == "1" ]; then
