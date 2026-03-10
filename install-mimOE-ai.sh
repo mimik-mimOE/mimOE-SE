408cbb9c807f57e435ad0d761048c664f030dbde
linminjie <37422026+linminjie@users.noreply.github.com>
2026-03-10 10:56:17 -0700

    Update install-mimOE-ai.sh

--- install-mimOE-ai.sh
+++ install-mimOE-ai.sh
@@ -243,7 +243,7 @@ enforce_empty_install_dir() {
 
 
 # Configuration
-VERSION="3.22.0"
+VERSION="3.20.2"
 API_KEY="1234"
 DEFAULT_MODEL_ID="smollm2-360m"
 DEFAULT_MODEL_URL="https://huggingface.co/lmstudio-community/SmolLM2-360M-Instruct-GGUF/resolve/main/SmolLM2-360M-Instruct-Q8_0.gguf?download=true"
